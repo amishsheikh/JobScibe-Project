1acdb1a431780ca87ed7592b4fc6a0c29fea21c2
amishseikh <sheikhamish20@gmail.com>
2025-11-06 12:37:08 +0530

    Changes in Feature.jsx

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Layout/Navbar.jsx"; // Navbar component
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
-import Features from "./pages/feature.jsx";
+import Features from "./pages/Feature.jsx";
 import About from "./components/AboutPage.jsx";
 import ContentPage from "./components/ContactPage.jsx"; // Contact / Content page
 
@@ -19,7 +19,8 @@ function AppRoutes() {
           <Route path="/" element={<Home />} />
           <Route path="/features" element={<Features />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContentPage />} /> {/* Correct element */}
+          <Route path="/contact" element={<ContentPage />} />{" "}
+          {/* Correct element */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
